43985a55f27725f97706c066e0825daa7bedd074
Pphoom-kit 3 <88782114+PPhoomkmitl@users.noreply.github.com>
2023-08-09 14:33:17 +0700

    Add files via upload

---
+++ Lab3/Program.cs
@@ -0,0 +1,14 @@
+﻿// 64050191 ภูมิรพี ศรีฟ้า
+
+//Implicit Type (var):
+var name = "Phoom"; 
+var age = 50;     
+
+// Cannot use dynamic type
+name = 1223; 
+age = "phoom";  
+
+// Here Error
+// error CS0029: Cannot implicitly convert type 'int' to 'string'
+// error CS0029: Cannot implicitly convert type 'string' to 'int'
+
